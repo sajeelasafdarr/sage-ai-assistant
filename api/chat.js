6f95f51554728a163e6fb4f0b962f41a4e80378a
Sajeela Safdar <sajeelasafdarr@gmail.com>
2026-06-17 02:59:06 -0700

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "Authorization": `Bearer ${process.env.GROQ_API_KEY}`
+        "Authorization": `Bearer ${process.env.Sage_API_Key}`
       },
       body: JSON.stringify({
         model: "llama3-8b-8192",
